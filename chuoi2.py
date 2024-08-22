dcbcfc88b29a6513e3b0499041985654f11dbedf
NguyenHongPhiYen <nguyenhongphiyen29@gmail.com>
2024-08-22 19:53:18 +0700

    Add files via upload

---
+++ chuoi2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 19:31:09 2024
+
+@author: Student
+"""
+
+chuoi=("i'm a cat")
+print(chuoi[0].upper()+chuoi[1:3]+chuoi[3]+chuoi[4].upper()+chuoi[5]+chuoi[6].upper()+chuoi[7:9])
+print(chuoi[0:9].upper())
+print(chuoi[0:2]+chuoi[2].upper()+chuoi[3:7]+chuoi[7:9].upper())
+print(chuoi[0].upper()+chuoi[1:9])
